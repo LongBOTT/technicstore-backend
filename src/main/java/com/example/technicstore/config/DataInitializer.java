2e0b03a0f21e3894e0ab2515033b89ea2c67b2ae
NguyenMinhThuan <95559097+NguyenMinhThuanK12@users.noreply.github.com>
2024-10-06 15:20:28 +0700

    add data

--- src/main/java/com/example/technicstore/config/DataInitializer.java
+++ src/main/java/com/example/technicstore/config/DataInitializer.java
@@ -187,8 +187,7 @@ public class DataInitializer implements CommandLineRunner {
 
         // region attribute
         seedDataAttribute("Màu sắc");
-        seedDataAttribute("Bộ nhớ");
-        seedDataAttribute("RAM");
+        seedDataAttribute("Dung lượng");
         //endregion
 
         // region brand
@@ -264,7 +263,7 @@ public class DataInitializer implements CommandLineRunner {
         // endregion
 
         // region warranty
-        seedDataWarranty("Bảo hành 10 ngày", "Ngày", 10);
+        seedDataWarranty("Không bảo hành", "Không", 0);
         seedDataWarranty("Bảo hành 10 ngày", "Ngày", 10);
         seedDataWarranty("Bảo hành 1 tháng", "Tháng", 1);
         seedDataWarranty("Bảo hành 6 tháng", "Tháng", 6);
@@ -285,133 +284,258 @@ public class DataInitializer implements CommandLineRunner {
         // region product
         // Điện thoại
         //Iphone
-        seedDataProduct("Iphone 14", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 14 Pro", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 14 Pro Max", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 15", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 15 Pro", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 15 Pro Max", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 16", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 16 Pro", "Điện thoại", 0.5, 1, 1, 1, 1);
-        seedDataProduct("Iphone 16 Pro Max", "Điện thoại", 0.5, 1, 1, 1, 1);
+        seedDataProduct("Iphone 14", "Điện thoại", 0.5, 1, 1, 1 );
+        seedDataProduct("Iphone 14 Pro Max", "Điện thoại", 0.5, 1, 1, 1);
+        seedDataProduct("Iphone 15", "Điện thoại", 0.5, 1, 1, 1);
+        seedDataProduct("Iphone 15 Pro Max", "Điện thoại", 0.5, 1, 1, 1);
         //Samsung
-        seedDataProduct("Samsung Galaxy S21", "Điện thoại", 0.5, 2, 1, 2, 1);
-        seedDataProduct("Samsung Galaxy S21 Ultra", "Điện thoại", 0.5, 2, 1, 2, 1);
-        seedDataProduct("Samsung Galaxy S21 Plus", "Điện thoại", 0.5, 2, 1, 2, 1);
-        seedDataProduct("Samsung Galaxy A55", "Điện thoại", 0.5, 2, 1, 2, 1);
-        seedDataProduct("Samsung Galaxy Z Fold6", "Điện thoại", 0.5, 2, 1, 2, 1);
-        seedDataProduct("Samsung Galaxy M54", "Điện thoại", 0.5, 2, 1, 2, 1);
+        seedDataProduct("Samsung Galaxy S21 FE 5G", "Điện thoại", 0.5, 2, 1,  1);
+        seedDataProduct("Samsung Galaxy A55 5G", "Điện thoại", 0.5, 2, 1,  1);
+        seedDataProduct("Samsung Galaxy Z Fold6", "Điện thoại", 0.5, 2, 1,  1);
+        seedDataProduct("Samsung Galaxy M54", "Điện thoại", 0.5, 2, 1,  1);
         //Xiaomi
-        seedDataProduct("Xiaomi Mi 11", "Điện thoại", 0.5, 3, 1, 6, 1);
-        seedDataProduct("Xiaomi Mi 11 Ultra", "Điện thoại", 0.5, 3, 1, 6, 1);
-        seedDataProduct("Xiaomi Mi 11 Pro", "Điện thoại", 0.5, 3, 1, 6, 1);
-        seedDataProduct("Xiaomi Redmi Note 13", "Điện thoại", 0.5, 3, 1, 6, 1);
-        seedDataProduct("Xiaomi Redmi Note 13 Pro", "Điện thoại", 0.5, 3, 1, 6, 1);
+        seedDataProduct("Xiaomi Mi 11", "Điện thoại", 0.5, 3, 1,  1);
+        seedDataProduct("Xiaomi Mi 11 Pro", "Điện thoại", 0.5, 3, 1,  1);
+        seedDataProduct("Xiaomi Redmi Note 13", "Điện thoại", 0.5, 3, 1,  1);
+        seedDataProduct("Xiaomi Redmi Note 13 Pro", "Điện thoại", 0.5, 3, 1, 1);
         //Oppo
-        seedDataProduct("Oppo Reno 7", "Điện thoại", 0.5, 4, 1, 4, 1);
-        seedDataProduct("Oppo Reno 7 Pro", "Điện thoại", 0.5, 4, 1, 4, 1);
+        seedDataProduct("Oppo Reno 7", "Điện thoại", 0.5, 4, 1,  1);
+        seedDataProduct("Oppo Reno 7 Pro", "Điện thoại", 0.5, 4, 1,  1);
 
 
         // Laptop
         // Thương hiệu Dell
-        seedDataProduct("Laptop Dell Inspiron 15 3520 i5 1235U", "Laptop", 1.5, 6, 2, 3, 2);
-        seedDataProduct("Laptop Dell Inspiron 15 3520 i7 1235U", "Laptop", 1.5, 6, 2, 3, 2);
-        seedDataProduct("Laptop Dell Inspiron 15 3530 i5 1334U", "Laptop", 1.5, 6, 2, 3, 2);
-        seedDataProduct("Laptop Dell Vostro 3520 i5 1235U", "Laptop", 1.5, 6, 2, 3, 2);
-        seedDataProduct("Laptop Dell Vostro 3530 i5 1335U", "Laptop", 1.5, 6, 2, 3, 2);
+        seedDataProduct("Laptop Dell Inspiron 15 3520 i5 1235U", "Laptop", 1.5, 6, 2,  2);
+        seedDataProduct("Laptop Dell Inspiron 15 3530 i5 1334U", "Laptop", 1.5, 6, 2,  2);
+        seedDataProduct("Laptop Dell Vostro 3520 i5 1235U", "Laptop", 1.5, 6, 2,  2);
+        seedDataProduct("Laptop Dell Vostro 3530 i5 1335U", "Laptop", 1.5, 6, 2,  2);
 
 
         // Thương hiệu HP
-        seedDataProduct("Laptop HP 15 fd0234TU i5 1334U", "Laptop", 1.5, 7, 2, 4, 2);
-        seedDataProduct("Laptop HP Pavilion 15 eg2081TU i5 1240P", "Laptop", 1.5, 7, 2, 4, 2);
-        seedDataProduct("Laptop HP 15 fd0303TU i3 1315U", "Laptop", 1.5, 7, 2, 4, 2);
-        seedDataProduct("Laptop HP 240 G9 i5 1235U/16GB/512GB/Win11", "Laptop", 1.5, 7, 2, 4, 2);
-        seedDataProduct("Laptop HP 240 G8 i3 1115G4/8GB/512GB/Win11", "Laptop", 1.5, 7, 2, 4, 2);
-        seedDataProduct("Laptop HP 15 fc0085AU R5 7430U/16GB/512GB/Win11", "Laptop", 1.5, 7, 2, 4, 2);
-        seedDataProduct("Laptop HP 15 fd0015TU i7 1355U/16GB/512GB/Win11", "Laptop", 1.5, 7, 2, 4, 2);
+        seedDataProduct("Laptop HP 15 fd0234TU i5 1334U", "Laptop", 1.5, 7, 2, 2);
+        seedDataProduct("Laptop HP Pavilion 15 eg2081TU i5 1240P", "Laptop", 1.5, 7, 2,  2);
+        seedDataProduct("Laptop HP 15 fd0303TU i3 1315U", "Laptop", 1.5, 7, 2,  2);
+        seedDataProduct("Laptop HP 240 G9 i5 1235U/16GB/512GB/Win11", "Laptop", 1.5, 7, 2,  2);
 
         // Thể loại Tablet
         // Thương hiệu Iphone
-        seedDataProduct("iPad Air 6 M2 11 inch WiFi 128GB", "Máy tính bảng", 1.0, 12, 3, 4, 2);
-        seedDataProduct("iPad Pro M4 11 inch WiFi 256GB", "Máy tính bảng", 1.0, 12, 3, 4, 2);
+        seedDataProduct("iPad Air 6 M2 11 inch WiFi 128GB", "Máy tính bảng", 1.0, 12, 3,  2);
+        seedDataProduct("iPad Pro M4 11 inch WiFi 256GB", "Máy tính bảng", 1.0, 12, 3,  2);
 
         // Thương hiệu Samsung
-        seedDataProduct("Samsung Galaxy Tab A9+ 5G 4GB/64GB", "Máy tính bảng", 1.0, 2, 3, 4, 2);
-        seedDataProduct("Samsung Galaxy Tab A9 4G 4GB/64GB", "Máy tính bảng", 1.0, 2, 3, 4, 2);
+        seedDataProduct("Samsung Galaxy Tab A9+ 5G 4GB/64GB", "Máy tính bảng", 1.0, 2, 3,  2);
+        seedDataProduct("Samsung Galaxy Tab A9 4G 4GB/64GB", "Máy tính bảng", 1.0, 2, 3,  2);
 
         //Thương hiệu Levono
-        seedDataProduct("Lenovo Tab Plus WiFi 8GB/256GB", "Máy tính bảng", 1.0, 8, 3, 5, 2);
+        seedDataProduct("Lenovo Tab Plus WiFi 8GB/256GB", "Máy tính bảng", 1.0, 8, 3,  2);
 
         // Thương hiệu Xiaomi
-        seedDataProduct("Xiaomi Redmi Pad SE 8.7 WiFi 4GB/64GB", "Máy tính bảng", 1.0, 3, 3, 3, 2);
-        seedDataProduct("Xiaomi Redmi Pad Pro WiFi 8GB/128GB", "Máy tính bảng", 1.0, 3, 3, 3, 2);
+        seedDataProduct("Xiaomi Redmi Pad SE 8.7 WiFi 4GB/64GB", "Máy tính bảng", 1.0, 3, 3,  2);
+        seedDataProduct("Xiaomi Redmi Pad Pro WiFi 8GB/128GB", "Máy tính bảng", 1.0, 3, 3,  2);
 
         // Thể loại Tai nghe
         // Thương hiệu Apple
-        seedDataProduct("Bluetooth AirPods Pro Gen 2 MagSafe Charge (USB-C) Apple MTJV3", "Tai nghe", 0.1, 16, 6, 1, 1);
-        seedDataProduct("Tai nghe Có Dây Apple MMTN2", "Tai nghe", 0.1, 16, 6, 1, 1);
-        seedDataProduct("Tai nghe Có Dây Apple MTJY3", "Tai nghe", 0.1, 16, 6, 1, 1);
-        seedDataProduct("Tai nghe Bluetooth AirPods 3 Lightning Charge Apple MPNY3", "Tai nghe", 0.1, 16, 6, 1, 1);
+        seedDataProduct("Bluetooth AirPods Pro Gen 2 MagSafe Charge (USB-C) Apple MTJV3", "Tai nghe", 0.1, 16, 6,  1);
+        seedDataProduct("Tai nghe Có Dây Apple MMTN2", "Tai nghe", 0.1, 16, 6,  1);
+        seedDataProduct("Tai nghe Có Dây Apple MTJY3", "Tai nghe", 0.1, 16, 6,  1);
+        seedDataProduct("Tai nghe Bluetooth AirPods 3 Lightning Charge Apple MPNY3", "Tai nghe", 0.1, 16, 6,  1);
 
         // Thương hiệu Samsung
-        seedDataProduct("Tai nghe TWS Samsung Galaxy Buds3 R530N", "Tai nghe", 0.1, 2, 6, 2, 1);
-        seedDataProduct("Tai nghe TWS Samsung Galaxy Buds3 Pro R630N", "Tai nghe", 0.1, 2, 6, 2, 1);
-        seedDataProduct("Tai nghe Bluetooth True Wireless Samsung Galaxy Buds FE R400N", "Tai nghe", 0.1, 2, 2, 5, 1);
+        seedDataProduct("Tai nghe TWS Samsung Galaxy Buds3 R530N", "Tai nghe", 0.1, 2, 6,  1);
+        seedDataProduct("Tai nghe TWS Samsung Galaxy Buds3 Pro R630N", "Tai nghe", 0.1, 2, 6,  1);
+        seedDataProduct("Tai nghe Bluetooth True Wireless Samsung Galaxy Buds FE R400N", "Tai nghe", 0.1, 2, 2, 1);
 
 
         // Thương hiệu Xiaomi
-        seedDataProduct("Tai nghe Bluetooth True Wireless Xiaomi Redmi Buds 6 Play", "Tai nghe", 0.1, 3, 6, 6, 1);
-        seedDataProduct("Tai nghe Bluetooth TWS Xiaomi Redmi Buds 6 Lite", "Tai nghe", 0.1, 3, 6, 6, 1);
-        seedDataProduct("Tai nghe Bluetooth True Wireless Xiaomi Redmi Buds 5", "Tai nghe", 0.1, 3, 6, 6, 1);
+        seedDataProduct("Tai nghe Bluetooth True Wireless Xiaomi Redmi Buds 6 Play", "Tai nghe", 0.1, 3, 6, 1);
+        seedDataProduct("Tai nghe Bluetooth TWS Xiaomi Redmi Buds 6 Lite", "Tai nghe", 0.1, 3, 6,  1);
+        seedDataProduct("Tai nghe Bluetooth True Wireless Xiaomi Redmi Buds 5", "Tai nghe", 0.1, 3, 6,  1);
 
         // Thể loại Smartwatch
         // Thương hiệu SamSung
-        seedDataProduct("Đồng hồ thông minh Samsung Galaxy Watch7 40mm", "Smartwatch", 0.2, 2, 4, 2, 1);
-        seedDataProduct("Đồng hồ thông minh Galaxy Watch Ultra LTE 47mm", "Smartwatch", 0.2, 2, 4, 2, 1);
-        seedDataProduct("Đồng hồ thông minh Samsung Galaxy Watch7 44mm", "Smartwatch", 0.2, 2, 4, 2, 1);
+        seedDataProduct("Đồng hồ thông minh Samsung Galaxy Watch7 40mm", "Smartwatch", 0.2, 2, 4,1);
+        seedDataProduct("Đồng hồ thông minh Galaxy Watch Ultra LTE 47mm", "Smartwatch", 0.2, 2, 4,1);
+        seedDataProduct("Đồng hồ thông minh Samsung Galaxy Watch7 44mm", "Smartwatch", 0.2, 2, 4,1);
 
         // Thương hiệu Xiaomi
-        seedDataProduct("Vòng đeo tay thông minh Mi Band 9", "Smartwatch", 0.2, 6, 5, 6, 1);
-        seedDataProduct("Vòng đeo tay thông minh Mi Band 8 Pro", "Smartwatch", 0.2, 6, 5, 6, 1);
-        seedDataProduct("Đồng hồ thông minh Xiaomi Watch S3 47mm", "Smartwatch", 0.2, 6, 5, 6, 1);
+        seedDataProduct("Vòng đeo tay thông minh Mi Band 9", "Smartwatch", 0.2, 6, 5,  1);
+        seedDataProduct("Vòng đeo tay thông minh Mi Band 8 Pro", "Smartwatch", 0.2, 6, 5,  1);
+        seedDataProduct("Đồng hồ thông minh Xiaomi Watch S3 47mm", "Smartwatch", 0.2, 6, 5, 1);
 
         // Thể loại Sạc dự phòng
         // Thương hiệu Samsung
-        seedDataProduct("Sạc dự phòng Samsung 10000mAh EB-P1100C", "Sạc dự phòng", 0.3, 2, 6, 7, 1);
+        seedDataProduct("Sạc dự phòng Samsung 10000mAh EB-P1100C", "Sạc dự phòng", 0.3, 2, 6,  1);
 
         // Thể loại ốp lưng
         // Thương hiệu Apple
-        seedDataProduct("Ốp lưng iPhone 13 Pro Max", "Ốp lưng", 0.1, 16, 7, 1, 1);
-        seedDataProduct("Ốp lưng iPhone 13 Pro", "Ốp lưng", 0.1, 16, 7, 1, 1);
+        seedDataProduct("Ốp lưng iPhone 13 Pro Max", "Ốp lưng", 0.1, 16, 7,  9);
+        seedDataProduct("Ốp lưng iPhone 13 Pro", "Ốp lưng", 0.1, 16, 7,  9);
 
         // Thương hiệu Samsung
-        seedDataProduct("Ốp lưng Samsung Galaxy S21 Ultra", "Ốp lưng", 0.1, 2, 7, 2, 1);
-        seedDataProduct("Ốp lưng Samsung Galaxy S21", "Ốp lưng", 0.1, 2, 7, 2, 1);
+        seedDataProduct("Ốp lưng Samsung Galaxy S21 Ultra", "Ốp lưng", 0.1, 2, 7,  9);
+        seedDataProduct("Ốp lưng Samsung Galaxy S21", "Ốp lưng", 0.1, 2, 7,  9);
 
         // Thương hiệu Xiaomi
-        seedDataProduct("Ốp lưng Xiaomi Redmi Note 13 Pro", "Ốp lưng", 0.1, 3, 7, 6, 1);
-        seedDataProduct("Ốp lưng Xiaomi Redmi Note 13", "Ốp lưng", 0.1, 3, 7, 6, 1);
+        seedDataProduct("Ốp lưng Xiaomi Redmi Note 13 Pro", "Ốp lưng", 0.1, 3, 7,  9);
+        seedDataProduct("Ốp lưng Xiaomi Redmi Note 13", "Ốp lưng", 0.1, 3, 7,  9);
 
         // Thể loại Chuột
         // Thương hiệu Dell
-        seedDataProduct("Chuột Dell MS3220W", "Chuột", 0.2, 6, 8, 3, 1);
-        seedDataProduct("Chuột Dell MS3220W", "Chuột", 0.2, 6, 8, 3, 1);
+        seedDataProduct("Chuột Dell MS3220W", "Chuột", 0.2, 6, 8,  1);
+        seedDataProduct("Chuột Dell MS3220W", "Chuột", 0.2, 6, 8,  1);
 
         // Thương hiệu HP
-        seedDataProduct("Chuột HP X1000", "Chuột", 0.2, 7, 8, 4, 1);
-        seedDataProduct("Chuột HP X1000", "Chuột", 0.2, 7, 8, 4, 1);
+        seedDataProduct("Chuột HP X1000", "Chuột", 0.2, 7, 8,  1);
+        seedDataProduct("Chuột HP X1000", "Chuột", 0.2, 7, 8,  1);
 
         // Thương hiệu Lenovo
-        seedDataProduct("Chuột Lenovo M120 Pro", "Chuột", 0.2, 8, 8, 5, 1);
+        seedDataProduct("Chuột Lenovo M120 Pro", "Chuột", 0.2, 8, 8,  1);
 
         // Thể loại Bàn phím
         // Thương hiệu Apple
-        seedDataProduct("Bàn phím Apple Magic Keyboard", "Bàn phím", 0.3, 16, 9, 1, 1);
+        seedDataProduct("Bàn phím Apple Magic Keyboard", "Bàn phím", 0.3, 16, 9,  1);
         //Thương hiệu xiaomi
-        seedDataProduct("Bàn phím Xiaomi Mi Wireless Keyboard", "Bàn phím", 0.3, 3, 9, 6, 1);
+        seedDataProduct("Bàn phím Xiaomi Mi Wireless Keyboard", "Bàn phím", 0.3, 3, 9,  1);
 
         // endregion
+
+        // region variant
+        // Điện thoại
+        //Iphone
+        // iphone14
+        seedDataVariant("iphone14", 17090000.0, 10, 1);
+        seedDataVariant("iphone14", 19090000.0, 10, 1);
+        seedDataVariant("iphone14", 24090000.0, 10, 1);
+
+        //iphone 14 promax
+        seedDataVariant("iphone14promax", 25090000.0, 10, 2);
+        seedDataVariant("iphone14promax", 27090000.0, 10, 2);
+        seedDataVariant("iphone14promax", 32090000.0, 10, 2);
+
+        // iphone 15
+        seedDataVariant("iphone15", 18090000.0, 10, 3);
+        seedDataVariant("iphone15", 20090000.0, 10, 3);
+        seedDataVariant("iphone15", 25090000.0, 10, 3);
+
+        // iphone 15 promax
+        seedDataVariant("iphone15promax", 28090000.0, 10, 4);
+
+        // SamSung
+        // Samsung Galaxy S21
+        seedDataVariant("SamsungGalaxyS21", 15090000.0, 10, 5);
+        seedDataVariant("SamsungGalaxyS21", 20090000.0, 10, 5);
+
+        // Samsung Galaxy A55
+        seedDataVariant("SamsungGalaxyA55G", 9690000.0, 10, 6);
+        seedDataVariant("SamsungGalaxyA55G", 10790000.0, 10, 6);
+
+        // Samsung Galaxy Z Fold6
+        seedDataVariant("SamsungGalaxyZFold6", 39990000.0, 10, 7);
+        seedDataVariant("SamsungGalaxyZFold6", 43990000.0, 10, 7);
+
+        // Samsung Galaxy M54 5G
+        seedDataVariant("SamsungGalaxyM54G", 7990000.0, 10, 8);
+
+        // Xiaomi
+        // Xiaomi Mi 11
+
+        // end region
+      //  region variant_attribute
+
+        // điện thoại iphone 14 dung lượng 128 GB có 3 màu Đen, Trắng, Xanh giá 17.090.000
+        seedDataVariantAtribute("Đen", 1, 1);
+        seedDataVariantAtribute("Trắng", 1, 1);
+        seedDataVariantAtribute("Xanh", 1, 1);
+        seedDataVariantAtribute("128GB", 2, 1);
+
+        // điện thoại iphone 14 dung lượng 256 GB có 3 màu Đen, Trắng, Xanh giá 19.090.000
+        seedDataVariantAtribute("Đen", 1, 2);
+        seedDataVariantAtribute("Trắng", 1, 2);
+        seedDataVariantAtribute("Xanh", 1, 2);
+        seedDataVariantAtribute("256GB", 2, 2);
+
+        //điện thoại iphone 14 dung lượng 256 GB có 3 màu Đen, Trắng, Xanh giá 24.090.000
+        seedDataVariantAtribute("Đen", 1, 3);
+        seedDataVariantAtribute("Trắng", 1, 3);
+        seedDataVariantAtribute("Xanh", 1, 3);
+        seedDataVariantAtribute("512GB", 2, 3);
+
+        // iphone 14 Pro Max dung lượng 128 GB có 3 màu Đen, Trắng, Xanh giá 25.090.000
+        seedDataVariantAtribute("Đen", 1, 4);
+        seedDataVariantAtribute("Trắng", 1, 4);
+        seedDataVariantAtribute("Xanh", 1, 4);
+        seedDataVariantAtribute("128GB", 2, 4);
+
+        // iphone 14 Pro Max dung lượng 256 GB có 3 màu Đen, Trắng, Xanh giá 27.090.000
+        seedDataVariantAtribute("Đen", 1, 5);
+        seedDataVariantAtribute("Trắng", 1, 5);
+        seedDataVariantAtribute("Xanh", 1, 5);
+        seedDataVariantAtribute("256GB", 2, 5);
+
+        // iphone 14 Pro Max dung lượng 512 GB có 3 màu Đen, Trắng, Xanh giá 32.090.000
+        seedDataVariantAtribute("Đen", 1, 6);
+        seedDataVariantAtribute("Trắng", 1, 6);
+        seedDataVariantAtribute("Xanh", 1, 6);
+        seedDataVariantAtribute("512GB", 2, 6);
+
+        // iphone 15 dung lượng 128 GB có 2 màu Đen, Trắng giá 18.090.000
+        seedDataVariantAtribute("Đen", 1, 7);
+        seedDataVariantAtribute("Trắng", 1, 7);
+        seedDataVariantAtribute("128GB", 2, 7);
+
+        // iphone 15 dung lượng 256 GB có 2 màu Đen, Trắng giá 20.090.000
+        seedDataVariantAtribute("Đen", 1, 8);
+        seedDataVariantAtribute("Trắng", 1, 8);
+        seedDataVariantAtribute("256GB", 2, 8);
+
+        // iphone 15 dung lượng 512 GB có 2 màu Đen, Trắng giá 25.090.000
+        seedDataVariantAtribute("Đen", 1, 9);
+        seedDataVariantAtribute("Trắng", 1, 9);
+        seedDataVariantAtribute("512GB", 2, 9);
+
+        // iphone 15 Pro Max dung lượng 256 GB có 2 màu Đen, Trắng giá 28.090.000
+        seedDataVariantAtribute("Đen", 1, 10);
+        seedDataVariantAtribute("Trắng", 1, 10);
+        seedDataVariantAtribute("256GB", 2, 10);
+
+        // SamSung
+        // Samsung Galaxy S21 FE 5G dung lượng 8GB - 128 GB có 3 màu Trắng,Xám giá 15.090.000
+        seedDataVariantAtribute("Trắng", 1, 11);
+        seedDataVariantAtribute("Xám", 1, 11);
+        seedDataVariantAtribute("8GB - 128GB", 2, 11);
+
+        // Samsung Galaxy S21 dung lượng 256 GB có 3 màu Trắng,Xám giá 20.090.000
+        seedDataVariantAtribute("Trắng", 1, 12);
+        seedDataVariantAtribute("Xám", 1, 12);
+        seedDataVariantAtribute("8GB - 256GB", 2, 12);
+
+        //Samsung Galaxy A55 5G dung lượng 8GB -128GB có 2 màu Đen, Trắng giá 9.690.000
+        seedDataVariantAtribute("Đen", 1, 13);
+        seedDataVariantAtribute("Trắng", 1, 13);
+        seedDataVariantAtribute("8GB - 28GB", 2, 13);
+
+        //Samsung Galaxy A55 5G dung lượng 8GB -256 có 2 màu Đen, Trắng giá 10.790.000
+        seedDataVariantAtribute("Đen", 1, 14);
+        seedDataVariantAtribute("Trắng", 1, 14);
+        seedDataVariantAtribute("8GB - 256GB", 2, 14);
+
+        // Samsung Galaxy Z Fold6 dung lượng 12GB - 256GB có 2 màu Hồng, Xanh dương giá 39.990.000
+        seedDataVariantAtribute("Hồng", 1, 15);
+        seedDataVariantAtribute("Xanh dương", 1, 15);
+        seedDataVariantAtribute("12GB - 256GB", 2, 15);
+
+        // Samsung Galaxy Z Fold6 dung lượng 12GB - 512GB có 2 màu Hồng, Xanh dương giá 43.990.000
+        seedDataVariantAtribute("Hồng", 1, 16);
+        seedDataVariantAtribute("Xanh dương", 1, 16);
+        seedDataVariantAtribute("12GB - 512GB", 2, 16);
+
+        // Xiaomi
+
+
+        // end region
+
     }
+
 
     public void seedDataVariant(String image, Double price, int quantity, int product_id) {
         Variant variant = new Variant();
@@ -430,7 +554,7 @@ public class DataInitializer implements CommandLineRunner {
         variantValueRepository.save(variant_attribute);
     }
 
-    public void seedDataProduct(String name, String description, Double weight, int brand_id, int category_id, int supplier_id, int warranty_id) {
+    public void seedDataProduct(String name, String description, Double weight, int brand_id, int category_id, int warranty_id) {
         Product product = new Product();
         product.setName(name);
         product.setDescription(description);
