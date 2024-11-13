12f87279b585cce7317634a35a59c4cfaf88ccc5
NguyenMinhThuan <95559097+NguyenMinhThuanK12@users.noreply.github.com>
2024-11-13 20:10:11 +0700

    .

--- src/main/java/com/example/technicstore/service/CustomerService.java
+++ src/main/java/com/example/technicstore/service/CustomerService.java
@@ -29,9 +29,7 @@ public class CustomerService {
     public List<Customer> getCustomersByName(String name) {
         return customerRepository.findCustomerByNameContainsIgnoreCase(name);
     }
-    public Optional<Customer> findByPhone(String phone) {
-        return customerRepository.findCustomerByPhone(phone);
-    }
+
     public Customer createCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
